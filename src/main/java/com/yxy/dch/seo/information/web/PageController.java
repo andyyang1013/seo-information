ad9269c8365f4638b449109c526f2e035e27ad5b
yz <yz@daicaihang.com>
2018-08-06 10:13:37 +0800

    commit code

--- src/main/java/com/yxy/dch/seo/information/web/PageController.java
+++ src/main/java/com/yxy/dch/seo/information/web/PageController.java
@@ -90,7 +90,6 @@ public class PageController extends BaseController {
         List<ArticleVO> articleList = articleService.getArticlesByColNamePinyin(namePinyin);
         PageInfo<ArticleVO> pageInfo = new PageInfo<>(articleList);
         modelAndView.addObject("articleList", articleList);
-        logger.info("pageInfo======================={}",pageInfo);
         long pageCount = pageInfo.getTotal() / 10 == 0 ? 1 : pageInfo.getTotal() / 10;
         modelAndView.addObject("pageCount", pageCount);
         // 上一页码
@@ -148,7 +147,6 @@ public class PageController extends BaseController {
         List<ArticleVO> articleList = articleService.getArticlesByColNamePinyin(namePinyin);
         PageInfo<ArticleVO> pageInfo = new PageInfo<>(articleList);
         modelAndView.addObject("articleList", articleList);
-        logger.info("pageInfo======================={}",pageInfo);
         long pageCount = pageInfo.getTotal() / 10 == 0 ? 1 : pageInfo.getTotal() / 10;
         modelAndView.addObject("pageCount", pageCount);
         // 上一页码
